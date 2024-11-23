34c95aedfe3c3de7850ed31dce1f811ff83c405e
EC2 Default User <ec2-user@ip-172-31-17-45.ec2.internal>
2024-11-23 01:42:27 +0000

    midterm completed

--- skillfair/src/app/app.component.ts
+++ skillfair/src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+import { JumbotronComponent } from './jumbotron/jumbotron.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { FooterComponent } from './footer/footer.component';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,HeaderComponent,JumbotronComponent,CatalogueComponent,FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  
 })
 export class AppComponent {
   title = 'skillfair';
